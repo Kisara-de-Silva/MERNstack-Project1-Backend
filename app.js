8276ecebdccfc2c4b6236b44e1ccfd07fd6a071b
Kisara de Silva <manmithdesilva@gmail.com>
2025-06-30 11:21:26 +0530

    Updates in app and server

--- app.js
+++ app.js
@@ -13,4 +13,17 @@ app.use(
 
 app.use(express.json());
 
+app.get('/users', (req, res) => {
+    controller.getUsers(users => {
+        res.send(users);
+    });
+});
+
+app.get('user', (req, res) => {
+    const id = req.query.id;
+    controller.getUserById(id, user => {
+        res.send(user);
+    });
+});
+
 module.exports = app;
