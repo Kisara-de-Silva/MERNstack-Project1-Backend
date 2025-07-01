3f011be6b264bd54b7db418b4e9c47bc97ac777a
Kisara de Silva <manmithdesilva@gmail.com>
2025-07-01 14:01:36 +0530

    Making CRUD functions on the Backend

--- server.js
+++ server.js
@@ -1,7 +1,31 @@
-const app = require('./app');
+//const app = require('./app');
+const express = require('express');
+const app = express();
+const cors = require('cors');
 const port = 3001;
-const host = '127.0.0.1';
+const host = 'localhost';
+const mongoose = require('mongoose');
+const router = require('./router');
+
+app.use(cors()); 
+app.use(express.json());
+
+const uri = 'mongodb+srv://KMan:KMan2003@cluster0.xrjuzrs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+const connect = async () => {
+    try {
+        await mongoose.connect(uri);
+        console.log('Connected to MongoDB');
+    }
+    catch(error) {
+        console.log('MongoDB Error: ', error);
+    }
+};
+
+connect();
 
 const server = app.listen(3001, '127.0.0.1', () => {
     console.log(`Node server is listening to ${server.address().port}`)
 });
+
+app.use('/api', router);
